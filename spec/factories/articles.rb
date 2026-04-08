f90bc73b43e82e6040bf81182962439ca4fb5cad
takumi <sawajr2243@gmail.com>
2026-04-09 06:45:18 +0900

    ファクトリーボットの作成

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -11,13 +11,8 @@
 #
 FactoryBot.define do
   factory :article do
-    # タイトル：1〜30文字の範囲でランダムに生成（モデルの maximum: 30 に合わせました）
-    title { Faker::Lorem.characters(number: Random.new.rand(1..30)) }
-
-    # 本文：ランダムな文章を生成
-    body { Faker::Lorem.paragraph }
-
-    # 関連付け：記事を作成するときに、自動で投稿者（User）も作成されるようにします
+    title { Faker::Lorem.characters(number: 20) }
+    body { Faker::Lorem.sentence }
     association :user
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,8 @@
 FactoryBot.define do
   factory :user do
-    # 名前がないとバリデーションに落ちるので、Fakerで20文字以内を生成
-    name { Faker::Lorem.characters(number: Random.new.rand(1..20)) }
-
-    # 重複エラーを防ぐために連番にする
-    sequence(:email) {|n| "user_#{n}@example.com" }
-
-    # 確実に 8 文字以上の複雑なパスワードを作る
-    password { Faker::Internet.password(min_length: 8, max_length: 32, mix_case: true, special_characters: true) }
+    name { Faker::Name.name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 8, max_length: 32) }
     password_confirmation { password }
   end
 end
