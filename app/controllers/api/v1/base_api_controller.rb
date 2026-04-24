1abc5bfc05016fca9b2cf45baf9bca3b76c0b073
takumi <sawajr2243@gmail.com>
2026-04-24 18:03:12 +0900

    各メソッドのAPI実装

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::BaseApiController < ApplicationController
-  # ここに v1 の API 全体で使いたい共通設定を書いていきます
+  # ここに v1 の API 全体で使いたい共通設定を書く。
+  # データベースの最初の一人目を「今のログインユーザー」と見なす設定
+  def current_user
+    @current_user ||= User.first
+  end
 end
