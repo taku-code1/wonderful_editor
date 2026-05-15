3a961d769ca9e8e0732b49ec6a2b307f0a2c8edc
takumi <sawajr2243@gmail.com>
2026-05-15 23:17:29 +0900

    記事にステータスを追加しデータベースを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_04_07_182635) do
+ActiveRecord::Schema.define(version: 2026_05_07_182606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2026_04_07_182635) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "draft"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
