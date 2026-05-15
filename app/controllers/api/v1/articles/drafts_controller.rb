28de110bcb33a460826e0efc8181cd5304de5061
takumi <sawajr2243@gmail.com>
2026-05-16 00:06:01 +0900

    下書き記事の一覧/詳細のAPIを作成

---
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -0,0 +1,16 @@
+module Api::V1
+  class Articles::DraftsController < BaseApiController
+    # ログインしている人かチェック
+    before_action :authenticate_user!
+
+    def index
+      articles = current_user.articles.draft.order(updated_at: :desc)
+      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
+    end
+
+    def show
+      article = current_user.articles.draft.find(params[:id])
+      render json: article, serializer: Api::V1::ArticleSerializer
+    end
+  end
+end
