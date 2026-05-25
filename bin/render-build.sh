1092823a25aade74c3ad08d33df3b9a354c1b120
takumi <sawajr2243@gmail.com>
2026-05-25 23:18:15 +0900

    render.yaml修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,18 +2,7 @@
 # exit on error
 set -o errexit
 
-# ① 依存パッケージを完全に本番環境へインストール
-yarn install --production=false
-
-# ② Rubyのセットアップ
 bundle install
-
-# ③ Vue（Webpack）を確実に本番用にビルドする
-bundle exec rails webpacker:compile
-
-# ④ Railsのアセットをコンパイル
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-
-# ⑤ データベースのマイグレーション
-bundle exec rails db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
